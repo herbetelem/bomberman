1e7b20c41eac00bad4e5249e8d4540509aecdd51
StendWood <edwoodzer@gmail.com>
2020-07-30 21:19:41 +0200

    Player can drop bombs

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
 # coding: utf-8
 
+# IMPORTS
 import pygame
+
+# Code additionnel
+from bomb import Bomb
 
 # créer la classe joueur
 class Player(pygame.sprite.Sprite):
-    
+
     def __init__(self, game, x, y):
         super().__init__()
         self.game = game
@@ -14,7 +18,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+        # Créer le groupe des bombes
+        self.all_bombs = pygame.sprite.Group()
 
     # method de delacement
     #   je verifie la direction
@@ -39,3 +44,17 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.speed
             if self.game.check_collision(self, self.game.all_rocks):
                 self.rect.x -= self.speed
+
+    def drop_bomb(self):
+        """
+            Laisse le joueur poser une bombe au sol (Max : 3)
+        """
+        if len(self.all_bombs) < 3:
+            # Si le joueur n'a pas posé 3 bombes
+            # Pose une bombe
+            self.all_bombs.add(Bomb(self))
+        
+
+        # DEBUG #
+        # print(self.all_bombs)
+        # print(len(self.all_bombs))
